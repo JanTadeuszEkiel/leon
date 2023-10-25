cd35b2b63d7cb5d1bbe0be4b2a3316e144460e2a
Jan Ekiel <60410323+JanTadeuszEkiel@users.noreply.github.com>
2023-10-25 23:41:30 +0200

    Moisture is sending to the USB

--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
+let Soil_Moisture = 0
 basic.forever(function () {
+    basic.showString("S= " + Soil_Moisture)
     basic.pause(1000)
+})
+basic.forever(function () {
+    basic.pause(200)
+    Soil_Moisture = pins.analogReadPin(AnalogPin.P1)
+    serial.writeLine("" + (Soil_Moisture))
 })
